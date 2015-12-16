c680a8cdfa4477636ceb612a3af8cf29db85fb5a
Luca Realdi <luca.realdi@opencontent.it>
2015-12-16 14:35:08 +0100

    timing point

--- src/Opencontent/Sensor/Legacy/SearchService.php
+++ src/Opencontent/Sensor/Legacy/SearchService.php
@@ -16,15 +16,40 @@ class SearchService extends BaseSearchService
     protected $fieldsMapper = array (
         'internalId' => 'sensor_internalid_si',
         'subject' => 'sensor_subject_t',
+
         'type' => 'sensor_type_s',
         'status' => 'sensor_status_lk',
         'workflow_status' => 'sensor_workflow_status_lk',
         'privacy' => 'sensor_privacy_lk',
         'moderation' => 'sensor_moderation_lk',
+
         'coordinates' => 'sensor_coordinates_gpt',
         'address' => 'sensor_address_t',
+
         'expiration' => 'sensor_expiration_dt',
         'expiration_days' => 'sensor_expiration_days_i',
+
+        'open' => 'sensor_open_dt',
+        'open_timestamp' => 'sensor_open_timestamp_i',
+        'open_weekday' => 'sensor_open_weekday_si',
+        'read_timestamp' => 'sensor_read_timestamp_i',
+        'read' => 'sensor_read_dt',
+        'reading_time' => 'sensor_reading_time_i',
+        'assigned_timestamp' => 'sensor_assigned_timestamp_i',
+        'assigned' => 'sensor_assigned_dt',
+        'assigning_time' => 'sensor_assigning_time_i',
+        'fix_timestamp' => 'sensor_fix_timestamp_i',
+        'fix' => 'sensor_fix_dt',
+        'fixing_time' => 'sensor_fixing_time_i',
+        'close_timestamp' => 'sensor_close_timestamp_i',
+        'close' => 'sensor_close_dt',
+        'closing_time' => 'sensor_closing_time_i',
+
+        'open_read_time' => 'sensor_open_read_time_i',
+        'read_assign_time' => 'sensor_read_assign_time_i',
+        'assign_fix_time' => 'sensor_assign_fix_time_i',
+        'fix_close_time' => 'sensor_fix_close_time_i',
+
         'author_id' => 'sensor_author_id_i',
         'author_name' => 'sensor_author_name_t',
         'reporter_id' => 'sensor_reporter_id_i',
@@ -38,33 +63,23 @@ class SearchService extends BaseSearchService
         'owner_name_list' => 'sensor_owner_name_list_lk',
         'observer_id_list' => 'sensor_observer_id_list_lk',
         'observer_name_list' => 'sensor_observer_name_list_lk',
-        'open' => 'sensor_open_dt',
-        'open_timestamp' => 'sensor_open_timestamp_i',
-        'open_weekday' => 'sensor_open_weekday_si',
-        'read_timestamp' => 'sensor_read_timestamp_i',
-        'read_' => 'sensor_read_dt',
-        'reading_time' => 'sensor_reading_time_i',
-        'assigned_timestamp' => 'sensor_assigned_timestamp_i',
-        'assigned' => 'sensor_assigned_dt',
-        'assigning_time' => 'sensor_assigning_time_i',
         'history_owner_name' => 'sensor_history_owner_name_lk',
         'history_owner_id' => 'sensor_history_owner_id_lk',
-        'close_timestamp' => 'sensor_close_timestamp_i',
-        'close' => 'sensor_close_dt',
-        'closing_time' => 'sensor_closing_time_i',
+
         'area_name_list' => 'sensor_area_name_list_lk',
         'area_id_list' => 'sensor_area_id_list_lk',
         'category_name_list' => 'sensor_category_name_list_lk',
         'category_id_list' => 'sensor_category_id_list_lk',
-//        'user_*_has_read' => 'sensor_user_*_has_read_b',
-//        'user_*_unread_timelines' => 'sensor_user_*_unread_timelines_i',
-//        'user_*_timelines' => 'sensor_user_*_timelines_i',
-//        'user_*_unread_comments' => 'sensor_user_*_unread_comments_i',
-//        'user_*_comments' => 'sensor_user_*_comments_i',
-//        'user_*_unread_private_messages' => 'sensor_user_*_unread_private_messages_i',
-//        'user_*_private_messages' => 'sensor_user_*_private_messages_i',
-//        'user_*_unread_responses' => 'sensor_user_*_unread_responses_i',
-//        'user_*_responses' => 'sensor_user_*_responses_i'
+
+        'user_*_has_read' => 'sensor_user_*_has_read_b',
+        'user_*_unread_timelines' => 'sensor_user_*_unread_timelines_i',
+        'user_*_timelines' => 'sensor_user_*_timelines_i',
+        'user_*_unread_comments' => 'sensor_user_*_unread_comments_i',
+        'user_*_comments' => 'sensor_user_*_comments_i',
+        'user_*_unread_private_messages' => 'sensor_user_*_unread_private_messages_i',
+        'user_*_private_messages' => 'sensor_user_*_private_messages_i',
+        'user_*_unread_responses' => 'sensor_user_*_unread_responses_i',
+        'user_*_responses' => 'sensor_user_*_responses_i'
     );
 
     public function getSolrFields( Post $post )
@@ -103,6 +118,73 @@ class SearchService extends BaseSearchService
         if ( $post->author && $post->reporter )
             $data['sensor_behalf_b'] = ( $post->author->name != $post->reporter->name ) ? 'true' : 'false';
 
+        $data['sensor_open_dt'] = strftime( '%Y-%m-%dT%H:%M:%SZ', $post->published->format( 'U' ) );
+        $data['sensor_open_timestamp_i'] = $post->published->format( 'U' );
+        $data['sensor_open_weekday_si'] = $post->published->format( 'w' );
+
+        $read = $post->timelineItems->getByType( 'read' )->first();
+        if ( $read )
+        {
+            $data['sensor_read_timestamp_i'] = $read->published->format( 'U' );
+            $data['sensor_read_dt'] = strftime(
+                '%Y-%m-%dT%H:%M:%SZ',
+                $read->published->format( 'U' )
+            );
+            $interval = $post->published->diff( $read->published );
+            $data['sensor_reading_time_i'] = Utils::getDateIntervalSeconds( $interval );
+        }
+
+        $assignedList = $post->timelineItems->getByType( 'assigned' );
+        $assigned = $assignedList->first();
+        if ( $assigned )
+        {
+            $data['sensor_assigned_timestamp_i'] = $assigned->published->format( 'U' );
+            $data['sensor_assigned_dt'] = strftime(
+                '%Y-%m-%dT%H:%M:%SZ',
+                $assigned->published->format( 'U' )
+            );
+            $interval = $post->published->diff( $assigned->published );
+            $data['sensor_assigning_time_i'] = Utils::getDateIntervalSeconds( $interval );
+        }
+
+        $fix = $post->timelineItems->getByType( 'fixed' )->last();
+        if ( $fix )
+        {
+            $data['sensor_fix_timestamp_i'] = $fix->published->format( 'U' );
+            $data['sensor_fix_dt'] = strftime(
+                '%Y-%m-%dT%H:%M:%SZ',
+                $fix->published->format( 'U' )
+            );
+
+            $interval = $post->published->diff( $fix->published );
+            $data['sensor_fixing_time_i'] = Utils::getDateIntervalSeconds( $interval );
+        }
+
+        $close = $post->timelineItems->getByType( 'closed' )->last();
+        if ( $close )
+        {
+            $data['sensor_close_timestamp_i'] = $close->published->format( 'U' );
+            $data['sensor_close_dt'] = strftime(
+                '%Y-%m-%dT%H:%M:%SZ',
+                $close->published->format( 'U' )
+            );
+
+            $interval = $post->published->diff( $close->published );
+            $data['sensor_closing_time_i'] = Utils::getDateIntervalSeconds( $interval );
+        }
+
+        if ( isset( $data['sensor_reading_time_i'] ) )
+            $data['sensor_open_read_time_i'] = $data['sensor_reading_time_i'];
+
+        if ( isset( $data['sensor_reading_time_i'] ) && isset( $data['sensor_assigning_time_i'] ) )
+            $data['sensor_read_assign_time_i'] = $data['sensor_assigning_time_i'] - $data['sensor_reading_time_i'];
+
+        if ( isset( $data['sensor_assigning_time_i'] ) && isset( $data['sensor_fixing_time_i'] ) )
+            $data['sensor_assign_fix_time_i'] = $data['sensor_fixing_time_i'] - $data['sensor_assigning_time_i'];
+
+        if ( isset( $data['sensor_fixing_time_i'] ) && isset( $data['sensor_closing_time_i'] ) )
+            $data['sensor_fix_close_time_i'] = $data['sensor_closing_time_i'] - $data['sensor_fixing_time_i'];;
+
         $data['sensor_participant_id_list_lk'] = implode( ',', $post->participants->getParticipantIdList() );
         $participantNameList = array();
         foreach( $post->participants->participants as $participant )
@@ -126,35 +208,6 @@ class SearchService extends BaseSearchService
         foreach( $post->observers->participants as $participant )
             $participantNameList[] = $participant->name;
         $data['sensor_observer_name_list_lk'] = implode( ',', $participantNameList );
-
-        $data['sensor_open_dt'] = strftime( '%Y-%m-%dT%H:%M:%SZ', $post->published->format( 'U' ) );
-        $data['sensor_open_timestamp_i'] = $post->published->format( 'U' );
-        $data['sensor_open_weekday_si'] = $post->published->format( 'w' );
-
-        $read = $post->timelineItems->getByType( 'read' )->first();
-        if ( $read )
-        {
-            $data['sensor_read_timestamp_i'] = $read->published->format( 'U' );
-            $data['sensor_read_dt'] = strftime(
-                '%Y-%m-%dT%H:%M:%SZ',
-                $read->published->format( 'U' )
-            );
-            $interval = $post->published->diff( $read->published );
-            $data['sensor_reading_time_i'] = \OpenContent\Sensor\Legacy\Utils::getDateIntervalSeconds( $interval );
-        }
-
-        $assignedList = $post->timelineItems->getByType( 'assigned' );
-        $assigned = $assignedList->first();
-        if ( $assigned )
-        {
-            $data['sensor_assigned_timestamp_i'] = $assigned->published->format( 'U' );
-            $data['sensor_assigned_dt'] = strftime(
-                '%Y-%m-%dT%H:%M:%SZ',
-                $assigned->published->format( 'U' )
-            );
-            $interval = $post->published->diff( $assigned->published );
-            $data['sensor_assigning_time_i'] = \OpenContent\Sensor\Legacy\Utils::getDateIntervalSeconds( $interval );
-        }
         $ownerHistory = array();
         foreach( $assignedList->messages as $message )
         {
@@ -167,19 +220,6 @@ class SearchService extends BaseSearchService
         }
         $data['sensor_history_owner_name_lk'] = implode( ',', $ownerHistory );
         $data['sensor_history_owner_id_lk'] = implode( ',', array_keys( $ownerHistory ) );
-
-        $close = $post->timelineItems->getByType( 'closed' )->last();
-        if ( $close )
-        {
-            $data['sensor_close_timestamp_i'] = $close->published->format( 'U' );
-            $data['sensor_close_dt'] = strftime(
-                '%Y-%m-%dT%H:%M:%SZ',
-                $close->published->format( 'U' )
-            );
-
-            $interval = $post->published->diff( $close->published );
-            $data['sensor_closing_time_i'] = \OpenContent\Sensor\Legacy\Utils::getDateIntervalSeconds( $interval );
-        }
 
         $areaList = array();
         foreach( $post->areas as $area )

cae1000e2a1d27d72f336bf275922b4da0b81f6b
Luca Realdi <lr@opencontent.it>
2020-10-06 23:41:54 +0200

    Fix assign action logic

--- src/Opencontent/Sensor/Core/ActionDefinitions/AssignAction.php
+++ src/Opencontent/Sensor/Core/ActionDefinitions/AssignAction.php
@@ -65,7 +65,7 @@ class AssignAction extends ActionDefinition
             }
         }
 
-        //if (!empty($participantIds) || $type == Participant::TYPE_USER) {
+//        if (!empty($participantIds) || $type == Participant::TYPE_USER) {
             $currentApproverIds = $post->approvers->getParticipantIdListByType($type);
             $currentOwnerIds = $post->owners->getParticipantIdListByType($type);
             $makeOwnerIds = array_diff($participantIds, $currentOwnerIds, $currentApproverIds);
@@ -74,7 +74,7 @@ class AssignAction extends ActionDefinition
 //            if ($makeObserverIds == $currentOwnerIds && empty($makeOwnerIds)) {
 //                $repository->getLogger()->notice('Post is already assigned to ' . $type, array('make_owners' => $makeOwnerIds));
 //            }
-        //}
+//        }
 
         if (!$allowMultipleOwner && count($makeOwnerIds) > 1) {
             $makeOwnerId = array_shift($makeOwnerIds);
@@ -85,6 +85,7 @@ class AssignAction extends ActionDefinition
         return [
             'owners' => $makeOwnerIds,
             'observers' => $makeObserverIds,
+            'already_owners' => array_diff($participantIds, $makeOwnerIds),
         ];
     }
 
@@ -100,14 +101,24 @@ class AssignAction extends ActionDefinition
 
     public function run(Repository $repository, Action $action, Post $post, User $user)
     {
+        // controllo i cambiamenti per gli utenti
         $userChanges = $this->getParticipantChanges('participant_ids', Participant::TYPE_USER, $repository, $action, $post);
         $repository->getLogger()->debug('Change users in assign', $userChanges);
 
+        // controllo i cambiamenti per i gruppi
         if ($this->hasParameterDefinition('group_ids')) {
             $groupChanges = $this->getParticipantChanges('group_ids', Participant::TYPE_GROUP, $repository, $action, $post);
             $repository->getLogger()->debug('Change groups in assign', $groupChanges);
         }else{
-            $groupChanges = ['owners' => [], 'observers' => []];
+            $groupChanges = ['owners' => [], 'observers' => [], 'already_owners' => []];
+            if (!empty($userChanges['owners'])){
+                foreach ($userChanges['owners'] as $userId){
+                    $ownerGroups = $repository->getUserService()->loadUser($userId)->groups;
+                    $currentGroupOwnerIds = $post->owners->getParticipantIdListByType(Participant::TYPE_GROUP);
+                    $groupChanges['already_owners'] = array_intersect($currentGroupOwnerIds, $ownerGroups);
+                    $repository->getLogger()->debug('Change groups in assign', $groupChanges);
+                }
+            }
         }
 
         $roles = $repository->getParticipantService()->loadParticipantRoleCollection();
@@ -116,38 +127,49 @@ class AssignAction extends ActionDefinition
 
         $doRefresh = false;
         $owners = array_merge($userChanges['owners'], $groupChanges['owners']);
+        // aggiungo incaricati (utenti e gruppi)
         foreach ($owners as $id) {
             $repository->getParticipantService()->addPostParticipant($post, $id, $roleOwner);
             $doRefresh = true;
         }
 
+        // aggiungo osservatori (utenti e gruppi)
         $observers = array_merge($userChanges['observers'], $groupChanges['observers']);
         foreach ($observers as $id) {
             $repository->getParticipantService()->addPostParticipant($post, $id, $roleObserver);
             $doRefresh = true;
         }
 
+        // se ci sono nuovi utenti incaricati cambio lo stato del post in assegnato
         if (!empty($userChanges['owners'])) {
             $repository->getLogger()->debug('Found owner: make post assigned', ['post' => $post->id]);
             $repository->getPostService()->setPostWorkflowStatus($post, Post\WorkflowStatus::ASSIGNED);
+
+        // se non c'è alcun utente incaricato né osservatore riporto il post in da assegnare
         }elseif (!(empty($userChanges['owners']) && empty($userChanges['observers'])) && $post->workflowStatus->code == Post\WorkflowStatus::ASSIGNED){
             $repository->getLogger()->debug('No owners found: make post read', ['post' => $post->id]);
             $repository->getPostService()->setPostWorkflowStatus($post, Post\WorkflowStatus::READ);
-            $repository->getMessageService()->addTimelineItemByWorkflowStatus($post, Post\WorkflowStatus::READ);
+            //$repository->getMessageService()->addTimelineItemByWorkflowStatus($post, Post\WorkflowStatus::READ);
         }
 
+        // se ci sono modifiche agli utenti aggiungo un messaggio di timeline
         if (!empty($owners)){
-            //$repository->getLogger()->debug('Change workflow', $owners);
-            $repository->getMessageService()->addTimelineItemByWorkflowStatus($post, Post\WorkflowStatus::ASSIGNED, $owners);
+            $workflowOwners = array_merge($owners, $groupChanges['already_owners']);
+            $repository->getLogger()->debug('Set workflow status', $workflowOwners);
+            $repository->getMessageService()->addTimelineItemByWorkflowStatus($post, Post\WorkflowStatus::ASSIGNED, $workflowOwners);
+            $doRefresh = true;
         }
 
         if ($doRefresh) {
             $post = $repository->getPostService()->refreshPost($post);
         }
 
+        // se ci sono nuovi utenti incaricati emetto l'evento on_assign (anche per l'azione AutoAssignAction)
         if (!empty($userChanges['owners'])) {
-            $this->fireEvent($repository, $post, $user, array('owners' => $userChanges['owners']));
+            $this->fireEvent($repository, $post, $user, array('owners' => $userChanges['owners']), 'on_assign');
         }
+
+        // se ci sono nuovi gruppi incaricati e non ci sono nuovi utenti incaricati emetto l'evento on_assign (anche per l'azione AutoAssignAction)
         if (!empty($groupChanges['owners']) && empty($userChanges['owners'])) {
             $this->fireEvent($repository, $post, $user, array('owner_groups' => $groupChanges['owners']), 'on_group_assign');
         }

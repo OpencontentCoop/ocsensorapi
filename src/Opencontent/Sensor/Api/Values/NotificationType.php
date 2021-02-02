7be2ec42f054d12f3b900e955eeb91e006bdaed0
Luca Realdi <luca.realdi@opencontent.it>
2021-02-02 12:20:36 +0100

    Remove group as default target in observer notification

--- src/Opencontent/Sensor/Api/Values/NotificationType.php
+++ src/Opencontent/Sensor/Api/Values/NotificationType.php
@@ -17,7 +17,7 @@ class NotificationType extends Exportable
     public $template;
 
     public $targets = [
-        ParticipantRole::ROLE_OBSERVER => [Participant::TYPE_USER, Participant::TYPE_GROUP],
+        ParticipantRole::ROLE_OBSERVER => [Participant::TYPE_USER],
         ParticipantRole::ROLE_OWNER => [Participant::TYPE_USER, Participant::TYPE_GROUP],
         ParticipantRole::ROLE_APPROVER => [Participant::TYPE_USER],
         ParticipantRole::ROLE_AUTHOR => [Participant::TYPE_USER],

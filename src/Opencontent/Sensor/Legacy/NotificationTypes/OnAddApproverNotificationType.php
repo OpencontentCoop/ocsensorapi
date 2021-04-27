21e02c134e5007a1318e684d66fa574415f9c8fa
Luca Realdi <lr@opencontent.it>
2021-04-27 14:41:12 +0200

    Send OnAddApproverNotification also to users

--- src/Opencontent/Sensor/Legacy/NotificationTypes/OnAddApproverNotificationType.php
+++ src/Opencontent/Sensor/Legacy/NotificationTypes/OnAddApproverNotificationType.php
@@ -5,6 +5,8 @@ namespace Opencontent\Sensor\Legacy\NotificationTypes;
 
 use Opencontent\Sensor\Api\Values\NotificationType;
 use ezpI18n;
+use Opencontent\Sensor\Api\Values\Participant;
+use Opencontent\Sensor\Api\Values\ParticipantRole;
 
 class OnAddApproverNotificationType extends NotificationType implements TemplateAwareNotificationTypeInterface
 {
@@ -15,5 +17,6 @@ class OnAddApproverNotificationType extends NotificationType implements Template
         $this->identifier = 'on_add_approver';
         $this->name = ezpI18n::tr('sensor/notification', 'Coinvolgimento di un riferimento per il cittadino');
         $this->description = ezpI18n::tr('sensor/notification', 'Ricevi una notifica quando una tua segnalazione è assegnata a un nuovo riferimento per il cittadino');
+        $this->targets[ParticipantRole::ROLE_APPROVER] = [Participant::TYPE_USER, Participant::TYPE_GROUP];
     }
 }

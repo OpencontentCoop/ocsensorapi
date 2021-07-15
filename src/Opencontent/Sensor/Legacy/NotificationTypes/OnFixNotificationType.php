b97a737ef128c192b13b0b5bab6653a70b6665a7
Luca Realdi <lr@opencontent.it>
2021-07-15 18:44:15 +0200

    Hide OnFixNotification to standard user

--- src/Opencontent/Sensor/Legacy/NotificationTypes/OnFixNotificationType.php
+++ src/Opencontent/Sensor/Legacy/NotificationTypes/OnFixNotificationType.php
@@ -4,15 +4,19 @@ namespace Opencontent\Sensor\Legacy\NotificationTypes;
 
 use Opencontent\Sensor\Api\Values\NotificationType;
 use ezpI18n;
+use Opencontent\Sensor\Api\Values\ParticipantRole;
 
 class OnFixNotificationType extends NotificationType implements TemplateAwareNotificationTypeInterface
 {
     use TemplateTextHelperTrait;
+
+    public $group = 'operator';
 
     public function __construct()
     {
         $this->identifier = 'on_fix';
         $this->name = ezpI18n::tr('sensor/notification', 'Intervento terminato');
         $this->description = ezpI18n::tr('sensor/notification', "Ricevi una notifica quando un responsabile ha completato l'attività che riguarda una tua segnalazione");
+        $this->targets[ParticipantRole::ROLE_AUTHOR] = [];
     }
 }

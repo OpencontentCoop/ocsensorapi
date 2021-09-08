e989d50b2c9dc0d81c1496fabe06a6fdfbc484fd
Luca Realdi <lr@opencontent.it>
2021-09-08 15:31:31 +0200

    Avoid auto assign for default approver group

--- src/Opencontent/Sensor/Core/PermissionDefinitions/CanAutoAssign.php
+++ src/Opencontent/Sensor/Core/PermissionDefinitions/CanAutoAssign.php
@@ -10,15 +10,29 @@ class CanAutoAssign extends UserIs
 {
     public $identifier = 'can_auto_assign';
 
+    private $defaultApprovers;
+
+    public function __construct($defaultApprovers = [])
+    {
+        $this->defaultApprovers = $defaultApprovers;
+    }
+
     public function userHasPermission(User $user, Post $post)
     {
+        $isDefaultApprover = false;
+        foreach ($post->approvers as $approver){
+            if (in_array($approver->id, $this->defaultApprovers)){
+                $isDefaultApprover = true;
+            }
+        }
         return !$post->workflowStatus->is(Post\WorkflowStatus::CLOSED)
             && !$this->participantIs(ParticipantRole::ROLE_APPROVER, $user, $post)
             && (
                 ($this->userGroupIs(ParticipantRole::ROLE_OWNER, $user, $post)
                     && !$this->participantIs(ParticipantRole::ROLE_OWNER, $user, $post))
                 ||
-                ($this->userIs(ParticipantRole::ROLE_APPROVER, $user, $post)
+                (!$isDefaultApprover
+                    && $this->userIs(ParticipantRole::ROLE_APPROVER, $user, $post)
                     && !($this->userIs(ParticipantRole::ROLE_OWNER, $user, $post) && $post->workflowStatus->is(Post\WorkflowStatus::ASSIGNED)))
             );
     }
